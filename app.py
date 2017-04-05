6d75dc932fd19712f26e28f038cf60154f7e0d41
Yu <shaoyenyu@gmail.com>
2017-04-05 22:35:44 +0800

    test

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, We are Sunshine & Yu ~!"
+    return "Hello, We are Sunshine & Yu ~!\nP.S.任桀是胖子!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
